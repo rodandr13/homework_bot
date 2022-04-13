a80f233cfd0a0d35752c02e481f5c522b5943d7e
Andrei Roditelev <rodandr13@gmail.com>
2022-04-13 17:38:22 +0400

    павки

--- homework.py
+++ homework.py
@@ -12,6 +12,7 @@ from dotenv import load_dotenv
 from exceptions import (SendMessageError, EndpointAPIError, HomeworkJSONError,
                         EndpointHTTPStatusError, MissingTokenError, HomeworkPracticumError)
 
+
 load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv('practicum_token')
